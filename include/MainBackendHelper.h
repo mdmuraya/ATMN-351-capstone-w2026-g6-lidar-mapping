afa89bd023d6ce810346f00467e7e9812292eebc
Dennis Muraya <n01671663@humber.ca>
2026-01-25 20:12:49 -0500

    Made some changes

--- include/MainBackendHelper.h
+++ include/MainBackendHelper.h
@@ -6,35 +6,41 @@
 #include <QTimer>
 #include <QQmlEngine>
 
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+
 
 class MainBackendHelper : public QObject
 {
     Q_OBJECT
     //QML_ELEMENT
     //QML_SINGLETON
-public:
-    explicit MainBackendHelper(QObject *parent = nullptr);
-    ~MainBackendHelper();
+    public:
+        explicit MainBackendHelper(QObject *parent = nullptr);
+        ~MainBackendHelper();
 
-signals:
-    void requestPLCStatus();
+    signals:
+        void requestPLCStatus();
 
-public slots:
-    void onConnectToPLC();
-    void onStartScan();
-    void onResetSystem();
-    void onStopScan();
-    void onSafetyStop();
-    void onMoveToHome();
-    void onMoveLeft();
-    void onMoveRight();
-    void onMoveForward();
-    void onMoveBack();
-    void onRequestPLCStatus();
+    public slots:
+        void onConnectToPLC();
+        void onStartScan();
+        void onResetSystem();
+        void onStopScan();
+        void onSafetyStop();
+        void onMoveToHome();
+        void onMoveLeft();
+        void onMoveRight();
+        void onMoveForward();
+        void onMoveBack();
+        void onRequestPLCStatus();
 
-private:
-    std::unique_ptr<QTimer> _getPLCStatusTimer = nullptr;
-    QDateTime _dateTimeOnApplicationStart = QDateTime::currentDateTime();    
+    private:
+        QDateTime _dateTimeOnApplicationStart = QDateTime::currentDateTime();
+        std::unique_ptr<QTimer> _getPLCStatusTimer = nullptr;
+        std::unique_ptr<QTimer> _publishTimer = nullptr;
+        std::shared_ptr<rclcpp::Node> _rosNode = nullptr;
+        rclcpp::Publisher<std_msgs::msg::String>::SharedPtr _publisher = nullptr;
 };
 
 #endif // MAINBACKENDHELPER_H
